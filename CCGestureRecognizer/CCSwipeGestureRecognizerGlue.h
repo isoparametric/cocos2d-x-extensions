b9d43569a70a315a919130a09ea2a5c4f5be9491
Isoparametric <wow@snow.dti2.ne.jp>
2013-10-08 14:39:36 +0900

    いらない

--- CCGestureRecognizer/CCSwipeGestureRecognizerGlue.h
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: CCSwipeGestureRecognizer
-** Generated automatically by tolua++-1.0.92 on Tue Oct  8 11:30:35 2013.
-*/
-
-/* Exported function */
-TOLUA_API int  tolua_CCSwipeGestureRecognizer_open (lua_State* tolua_S);
-
